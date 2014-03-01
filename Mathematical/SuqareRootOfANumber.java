00a7b5909419c763991e7e0105437340506da8ee
eshwar2305 <eshwar2305@gmail.com>
2014-03-01 14:36:23 -0800

    1Mar14

---
+++ Mathematical/SuqareRootOfANumber.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author eshwar2305
+ *
+ */
+public class SuqareRootOfANumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
